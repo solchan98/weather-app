9dfb4bf59c6bf6b0544b13211510ae9023e28f81
solchan98 <dev.sol0427@gmail.com>
2022-05-09 20:00:52 +0900

    chore: 날씨 관련 SVG파일 추가 및 일부 리액트 컴포넌트화 진행

---
+++ src/assets/svgs/weather/index.ts
@@ -0,0 +1,5 @@
+export {ReactComponent as Sun } from './sun.svg';
+export {ReactComponent as DegreesCelCius} from './degrees-celcius.svg';
+export {ReactComponent as RainDrop} from './raindrop.svg';
+export {ReactComponent as CloudWind} from './cloud-wind.svg';
+export {ReactComponent as Marker} from './marker.svg';
